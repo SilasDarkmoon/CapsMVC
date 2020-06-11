1b46be0f9fea39f6d54a26b3357ecaf73acc7d24
Silas <hdss3609@gmail.com>
2020-06-11 21:03:26 +0800

    2019lts改动 —— 初步的 StackingMainCamera

---
+++ Runtime/StackingCamera/StackingMainCamera.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class StackingMainCamera : MonoBehaviour
+{
+    private Camera _Camera;
+    private UniversalAdditionalCameraData _CameraEx;
+
+    private void Awake()
+    {
+        _Camera = GetComponent<Camera>();
+        _CameraEx = GetComponent<UniversalAdditionalCameraData>();
+        if (_Camera && !_CameraEx)
+        {
+            _CameraEx = gameObject.AddComponent<UniversalAdditionalCameraData>();
+        }
+        _CameraEx.renderType = CameraRenderType.Base;
+    }
+
+    private void Update()
+    {
+        if (_Camera)
+        {
+            var stack = _CameraEx.cameraStack;
+            for (int i = stack.Count - 1; i >= 0; --i)
+            {
+                var old = stack[i];
+                if (!old)
+                {
+                    stack.RemoveAt(i);
+                }
+            }
+
+            var cameras = GetSceneCameras();
+            bool newcam = false;
+            for (int i = 0; i < cameras.Count; ++i)
+            {
+                var cam = cameras[i];
+                if (cam != _Camera && !stack.Contains(cam))
+                {
+                    if (cam.targetTexture == null)
+                    {
+                        var camex = cam.gameObject.GetComponent<UniversalAdditionalCameraData>();
+                        if (!camex)
+                        {
+                            camex = cam.gameObject.AddComponent<UniversalAdditionalCameraData>();
+                        }
+                        camex.renderType = CameraRenderType.Overlay;
+                        stack.Add(cam);
+                        newcam = true;
+                    }
+                }
+            }
+
+            if (newcam)
+            {
+                stack.Sort((cam1, cam2) =>
+                {
+                    if (cam1.depth == cam2.depth) return 0;
+                    else if (cam1.depth > cam2.depth) return 1;
+                    else return -1;
+                });
+            }
+        }
+    }
+
+    private static List<Camera> _SceneCameras = new List<Camera>();
+    public static List<Camera> GetSceneCameras()
+    {
+        var cams = _SceneCameras;
+        cams.Clear();
+        cams.AddRange(Object.FindObjectsOfType<Camera>());
+        return cams;
+    }
+}
