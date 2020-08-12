36ed556568dc008a82e32f930e346e9b7a0d7e06
Silas <hdss3609@gmail.com>
2020-08-12 19:09:30 +0800

    在存储prefab/scene时添加 StackingDynamicCamera

--- Editor/StackingCameraFix.cs
+++ Editor/StackingCameraFix.cs
@@ -3,13 +3,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
 
 using Object = UnityEngine.Object;
 
 namespace Capstones.UnityEngineEx
 {
+    [InitializeOnLoad]
     public static class StackingCameraFixEditor
     {
+        static StackingCameraFixEditor()
+        {
+            EditorSceneManager.sceneSaving += (scene, path) =>
+            {
+                FixScene(scene);
+            };
+        }
+
+        private static bool FixScene(Scene scene)
+        {
+            bool changed = false;
+            var roots = scene.GetRootGameObjects();
+            for (int j = 0; j < roots.Length; ++j)
+            {
+                var root = roots[j];
+                if (PrefabUtility.GetPrefabInstanceStatus(root) == PrefabInstanceStatus.NotAPrefab)
+                {
+                    var cams = root.GetComponentsInChildren<Camera>();
+                    for (int k = 0; k < cams.Length; ++k)
+                    {
+                        var cam = cams[k];
+                        if (PrefabUtility.GetPrefabInstanceStatus(cam) == PrefabInstanceStatus.NotAPrefab)
+                        {
+                            if (!cam.GetComponent<StackingMainCamera>() && !cam.GetComponent<StackingDynamicCamera>())
+                            {
+                                if (cam.targetTexture == null)
+                                {
+                                    cam.gameObject.AddComponent<StackingDynamicCamera>();
+                                    changed = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return changed;
+        }
+        private static bool FixPrefab(GameObject root)
+        {
+            bool changed = false;
+            var cams = root.GetComponentsInChildren<Camera>();
+            for (int k = 0; k < cams.Length; ++k)
+            {
+                var cam = cams[k];
+                if (PrefabUtility.GetPrefabInstanceStatus(cam) == PrefabInstanceStatus.NotAPrefab)
+                {
+                    if (!cam.GetComponent<StackingMainCamera>() && !cam.GetComponent<StackingDynamicCamera>())
+                    {
+                        if (cam.targetTexture == null)
+                        {
+                            cam.gameObject.AddComponent<StackingDynamicCamera>();
+                            changed = true;
+                        }
+                    }
+                }
+            }
+            if (changed)
+            {
+                PrefabUtility.SavePrefabAsset(root);
+            }
+            return changed;
+        }
+        private static bool FixPrefab(string path)
+        {
+            var prefab = AssetDatabase.LoadMainAssetAtPath(path) as GameObject;
+            if (prefab)
+            {
+                return FixPrefab(prefab);
+            }
+            return false;
+        }
+
         [MenuItem("Mods/Client Update Fix - Stacking Camera", priority = 100020)]
         private static void UpdateFixStackingCamera()
         {
@@ -25,33 +100,8 @@ namespace Capstones.UnityEngineEx
                     var asset = AssetDatabase.LoadMainAssetAtPath(path);
                     if (asset is SceneAsset)
                     {
-                        bool changed = false;
                         var scene = UnityEditor.SceneManagement.EditorSceneManager.OpenScene(path, UnityEditor.SceneManagement.OpenSceneMode.Additive);
-                        var roots = scene.GetRootGameObjects();
-                        for (int j = 0; j < roots.Length; ++j)
-                        {
-                            var root = roots[j];
-                            if (PrefabUtility.GetPrefabInstanceStatus(root) == PrefabInstanceStatus.NotAPrefab)
-                            {
-                                var cams = root.GetComponentsInChildren<Camera>();
-                                for (int k = 0; k < cams.Length; ++k)
-                                {
-                                    var cam = cams[k];
-                                    if (PrefabUtility.GetPrefabInstanceStatus(cam) == PrefabInstanceStatus.NotAPrefab)
-                                    {
-                                        if (!cam.GetComponent<StackingMainCamera>() && !cam.GetComponent<StackingDynamicCamera>())
-                                        {
-                                            if (cam.targetTexture == null)
-                                            {
-                                                cam.gameObject.AddComponent<StackingDynamicCamera>();
-                                                changed = true;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        if (changed)
+                        if (FixScene(scene))
                         {
                             UnityEditor.SceneManagement.EditorSceneManager.SaveScene(scene);
                         }
@@ -60,32 +110,30 @@ namespace Capstones.UnityEngineEx
                     }
                     else if (asset is GameObject)
                     {
-                        bool changed = false;
                         var root = asset as GameObject;
-                        var cams = root.GetComponentsInChildren<Camera>();
-                        for (int k = 0; k < cams.Length; ++k)
-                        {
-                            var cam = cams[k];
-                            if (PrefabUtility.GetPrefabInstanceStatus(cam) == PrefabInstanceStatus.NotAPrefab)
-                            {
-                                if (!cam.GetComponent<StackingMainCamera>() && !cam.GetComponent<StackingDynamicCamera>())
-                                {
-                                    if (cam.targetTexture == null)
-                                    {
-                                        cam.gameObject.AddComponent<StackingDynamicCamera>();
-                                        changed = true;
-                                    }
-                                }
-                            }
-                        }
-                        if (changed)
-                        {
-                            PrefabUtility.SavePrefabAsset(root);
-                        }
+                        FixPrefab(root);
                     }
                     else
                     {
                         Resources.UnloadAsset(asset);
+                    }
+                }
+            }
+        }
+
+        private class StackingCameraFixEditorPostprocessor : AssetPostprocessor
+        {
+            private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+            {
+                if (importedAssets != null)
+                {
+                    for (int i = 0; i < importedAssets.Length; ++i)
+                    {
+                        var asset = importedAssets[i];
+                        if (asset.EndsWith(".prefab"))
+                        {
+                            FixPrefab(asset);
+                        }
                     }
                 }
             }
