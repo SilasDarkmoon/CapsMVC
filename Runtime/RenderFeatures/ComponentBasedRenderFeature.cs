6326d8a03f9e117a1e80c576c127abc89fd62959
Silas <hdss3609@gmail.com>
2021-07-12 20:50:05 +0800

    ComponentBasedRenderFeature - 现在统一 ExecuteInEditorMode。现在可以给SceneViewCamera添加RenderFeature

--- Runtime/RenderFeatures/ComponentBasedRenderFeature.cs
+++ Runtime/RenderFeatures/ComponentBasedRenderFeature.cs
@@ -3,28 +3,39 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
+[ExecuteInEditMode]
 public abstract class ComponentBasedRenderFeature : MonoBehaviour
 {
+    public UnityEngine.CameraType TargetCameraType;
+
     protected Camera _Camera;
     public Camera Camera { get { return _Camera; } }
 
+    private void OnValidate()
+    {
+#if UNITY_EDITOR
+        UniversalRenderFeature.RemoveRenderFeature(this);
+        if (enabled && gameObject.activeInHierarchy)
+        {
+            //if (Application.isPlaying || GetType().GetCustomAttributes(typeof(ExecuteInEditMode), true).Length > 0)
+            {
+                Awake();
+                OnEnable();
+            }
+        }
+#endif
+    }
     protected virtual void Awake()
     {
         _Camera = GetComponent<Camera>();
     }
     private void OnEnable()
     {
-        if (_Camera)
-        {
-            UniversalRenderFeature.RegRenderFeature(this);
-        }
+        UniversalRenderFeature.RegRenderFeature(this);
     }
     private void OnDisable()
     {
-        if (_Camera)
-        {
-            UniversalRenderFeature.UnregRenderFeature(this);
-        }
+        UniversalRenderFeature.UnregRenderFeature(this);
     }
 
     public abstract void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData);
--- Runtime/RenderFeatures/UniversalRenderFeature.cs
+++ Runtime/RenderFeatures/UniversalRenderFeature.cs
@@ -7,33 +7,98 @@ using UnityEngine.Rendering.Universal;
 public class UniversalRenderFeature : ScriptableRendererFeature
 {
     protected static readonly Dictionary<Camera, List<ComponentBasedRenderFeature>> _RegisteredFeatures = new Dictionary<Camera, List<ComponentBasedRenderFeature>>();
+    protected static readonly List<List<ComponentBasedRenderFeature>> _RegisteredTypedFeatures = new List<List<ComponentBasedRenderFeature>>();
     public static void RegRenderFeature(ComponentBasedRenderFeature feature)
     {
-        List<ComponentBasedRenderFeature> list;
         var cam = feature.Camera;
-        if (!_RegisteredFeatures.TryGetValue(cam, out list))
+        if (cam)
         {
-            list = new List<ComponentBasedRenderFeature>();
-            _RegisteredFeatures.Add(cam, list);
+            List<ComponentBasedRenderFeature> list;
+            if (!_RegisteredFeatures.TryGetValue(cam, out list))
+            {
+                list = new List<ComponentBasedRenderFeature>();
+                _RegisteredFeatures.Add(cam, list);
+            }
+            if (!list.Contains(feature))
+            {
+                list.Add(feature);
+            }
         }
-        if (!list.Contains(feature))
+        else
         {
-            list.Add(feature);
+            var type = feature.TargetCameraType;
+            int tindex = (int)type;
+            while (_RegisteredTypedFeatures.Count <= tindex)
+            {
+                _RegisteredTypedFeatures.Add(new List<ComponentBasedRenderFeature>());
+            }
+            var list = _RegisteredTypedFeatures[tindex];
+            if (!list.Contains(feature))
+            {
+                list.Add(feature);
+            }
         }
     }
     public static void UnregRenderFeature(ComponentBasedRenderFeature feature)
     {
-        List<ComponentBasedRenderFeature> list;
-        var cam = feature.Camera;
-        if (_RegisteredFeatures.TryGetValue(cam, out list))
         {
+            List<ComponentBasedRenderFeature> list;
+            var cam = feature.Camera;
+            if (_RegisteredFeatures.TryGetValue(cam, out list))
+            {
+                list.Remove(feature);
+                if (list.Count == 0)
+                {
+                    _RegisteredFeatures.Remove(cam);
+                }
+            }
+        }
+        var type = feature.TargetCameraType;
+        int tindex = (int)type;
+        if (_RegisteredTypedFeatures.Count > tindex)
+        {
+            var list = _RegisteredTypedFeatures[tindex];
             list.Remove(feature);
-            if (list.Count == 0)
+        }
+        RemoveDeadRenderFeatures();
+    }
+    public static void RemoveRenderFeature(ComponentBasedRenderFeature feature)
+    {
+        var cams = _RegisteredFeatures.Keys.ToArray();
+        for (int i = 0; i < cams.Length; ++i)
+        {
+            var cam = cams[i];
+            if (!cam)
             {
                 _RegisteredFeatures.Remove(cam);
             }
+            else
+            {
+                var list = _RegisteredFeatures[cam];
+                for (int j = list.Count - 1; j >= 0; --j)
+                {
+                    if (feature == list[j])
+                    {
+                        list.RemoveAt(j);
+                    }
+                }
+                if (list.Count == 0)
+                {
+                    _RegisteredFeatures.Remove(cam);
+                }
+            }
         }
-        RemoveDeadRenderFeatures();
+        for (int i = 0; i < _RegisteredTypedFeatures.Count; ++i)
+        {
+            var list = _RegisteredTypedFeatures[i];
+            for (int j = list.Count - 1; j >= 0; --j)
+            {
+                if (feature == list[j])
+                {
+                    list.RemoveAt(j);
+                }
+            }
+        }
     }
     public static void RemoveDeadRenderFeatures()
     {
@@ -62,14 +127,40 @@ public class UniversalRenderFeature : ScriptableRendererFeature
                 }
             }
         }
+        for (int i = 0; i < _RegisteredTypedFeatures.Count; ++i)
+        {
+            var list = _RegisteredTypedFeatures[i];
+            for (int j = list.Count - 1; j >= 0; --j)
+            {
+                var feature = list[j];
+                if (!feature)
+                {
+                    list.RemoveAt(j);
+                }
+            }
+        }
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        var cam = renderingData.cameraData.camera;
-        List<ComponentBasedRenderFeature> list;
-        if (_RegisteredFeatures.TryGetValue(cam, out list))
         {
+            var cam = renderingData.cameraData.camera;
+            List<ComponentBasedRenderFeature> list;
+            if (_RegisteredFeatures.TryGetValue(cam, out list))
+            {
+                for (int i = 0; i < list.Count; ++i)
+                {
+                    var feature = list[i];
+                    feature.AddRenderPasses(renderer, ref renderingData);
+                }
+            }
+        }
+
+        var type = renderingData.cameraData.camera.cameraType;
+        int tindex = (int)type;
+        if (_RegisteredTypedFeatures.Count > tindex)
+        {
+            var list = _RegisteredTypedFeatures[tindex];
             for (int i = 0; i < list.Count; ++i)
             {
                 var feature = list[i];
