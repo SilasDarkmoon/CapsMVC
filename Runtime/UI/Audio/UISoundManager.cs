bd3b8d212518c8b51e422495e5a85d3121bde438
zhm <346629100@com>
2023-04-03 20:24:30 +0800

    #BQ-16162 【核心玩法卡顿】同步加载音频改成异步加载

--- Runtime/UI/Audio/UISoundManager.cs
+++ Runtime/UI/Audio/UISoundManager.cs
@@ -5,27 +5,16 @@ public static class UISoundManager
     private static float UISoundVolume = 1.0f;
 
     private static string category = "ui";
-
+    // 是否立即播放
+    public static bool IsInstantlyPlay = true;
     public static void Play(string file, float volume = -1f, bool loop = false)
     {
-        if (volume == 0f)
-        {
-            volume = UISoundVolume;
-        }
-
-        if (AudioManager.GetPlayer(category) == null)
-        {
-            AudioManager.CreatePlayer(category, true);
-        }
-        AudioManager.GetPlayer(category)
-            .PlayAudioInstantly("Game/Audio/UI/" + file, (float)volume, loop);
+        if (volume == 0f) volume = UISoundVolume;
+        if (AudioManager.GetPlayer(category) == null) AudioManager.CreatePlayer(category, true);
+        AudioManager.GetPlayer(category).PlayAudio("Game/Audio/UI/" + file, (float)volume, loop, IsInstantlyPlay);
     }
-
     public static void Stop()
     {
-        if (AudioManager.GetPlayer(category) != null)
-        {
-            AudioManager.GetPlayer(category).Stop();
-        }
+        if (AudioManager.GetPlayer(category) != null) AudioManager.GetPlayer(category).Stop();
     }
 }
